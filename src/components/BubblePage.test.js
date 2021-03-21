822edc730e0705ec829479e814fb03f50fb22ca9
Allison Castaneda <allraec@yahoo.com>
2021-03-21 18:51:58 -0500

    bubble page test

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,47 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+import ColorList from "./ColorList";
+import Bubbles from "./Bubbles";
 
-test("Renders BubblePage without errors", () => {
+const testColors = [
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff",
+    },
+    id: 1,
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc",
+    },
+    id: 2,
+  },
+  {
+    color: "aqua",
+    code: {
+      hex: "#00ffff",
+    },
+    id: 3,
+  }
+]
+
+test("Renders BubblePage without errors", async () => {
   // Finish this test
+  render(<BubblePage />)
 });
 
-test("Fetches data and renders the bubbles on mounting", () => {
+test("Fetches data and renders the bubbles on mounting", async () => {
   // Finish this test
+  render(<ColorList colors={testColors}/>)
+  render(<Bubbles colors={testColors}/>)
+
+  const color = screen.getByText(/aliceblue/i);
+  const color2 = screen.getByText(/limegreen/i);
+  expect(color).toBeInTheDocument();
+  expect(color2).toBeInTheDocument();
 });
 
 //Task List
